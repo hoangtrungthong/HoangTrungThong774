9d33c5d6b92d5c88d6ef672b524cfaec2445434a
hoang trung thong <hoangtrungthong0000@gmail.com>
2021-10-06 07:10:26 +0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HoangTrungThong774.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
